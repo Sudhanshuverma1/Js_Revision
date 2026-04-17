3c81b53787cf52953bb415ba43e6f73dbbf24c93
Sudhanshu Verma <sudhanshuuu01@gmail.com>
2026-04-17 13:11:53 +0530

    checking how its work

--- 03_Basics/0222.js
+++ 03_Basics/0222.js
@@ -1,14 +1,21 @@
- hello
+//  hello
 
- jddjsdf
- SVGDefsElement
- CDATASection
- DOMException
+//  jddjsdf
+//  SVGDefsElement
+//  CDATASection
+//  DOMException
 
 
-const name = 
+// const name = 
 
 
 
  
-03_Basics/0222.js
+// 03_Basics/0222.js
+// Fvlkdfkd
+
+// Sa 
+const arr = [10,20,20,111,]
+console.log (arr);
+
+console.log(arr.map(x => x*2))
