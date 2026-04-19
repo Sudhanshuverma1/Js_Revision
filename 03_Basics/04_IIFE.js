69dbe061854059802951d802b93defe044ee0e23
Sudhanshu Verma <sudhanshuuu01@gmail.com>
2026-04-19 13:11:47 +0530

    immediately invoked function expression about ...

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,48 @@
+/* Immediately Invoked Function Expressions (IIFE) are JavaScript functions that are executed immediately after they are defined. They are typically used to create a local scope for variables to prevent them from polluting the global scope. */
+
+(function sudhz(){
+    //NAME iife
+    console.log('DB CONNECTED');
+})();
+// sudhz()
+(function sudhz(){
+    console.log('DB CONNECTED');
+})();
+
+// another exxample
+let me = (function(){
+
+    let a = 10;
+    let b = 20;
+    return a + b;
+})();
+console.log(me);
+
+// exampel::: Arrow function
+ (() => {
+    console.log(`DATABASE 2 CONNECTED !`);
+    
+} )();
+
+
+
+// ( (name) =>{
+//     console.log(`DATABASE 2 CONNECTED ! ${name}`);
+    
+// } )('sudhanshu')
+
+
+
+let val1 = 10
+let val2 = 5
+function addNum( num1, num2){
+    let total = num1 + num2
+    return total
+}
+let result1 = addNum(val1,val2)
+let result2 = addNum (10,3)
+
+
+setTimeout(function () {
+  console.log("Hi");
+}, 1000);
