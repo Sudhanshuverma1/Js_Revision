8505ee198422a9a2e9bb0f8476ad0745451c9ca3
Sudhanshu Verma <sudhanshuuu01@gmail.com>
2026-04-20 03:32:51 +0530

    some issue

--- 03_Basics/0222.js
+++ 03_Basics/0222.js
@@ -21,3 +21,23 @@ console.log (arr);
 console.log(arr.map(x => x*2))
 
 //map
+
+const month = 3
+
+switch (month) {
+    case 1: 
+    console.log('January');
+    break;
+
+    case 2: 
+    console.log('February');
+    break;
+    case 3: 
+    console.log('March');
+    break;
+
+    default:
+        console.log('Default case match');
+        break;
+        
+}
